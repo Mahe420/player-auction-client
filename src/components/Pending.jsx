e3fff940286c05469a6a47f791ee38521977bef9
Mahe <mahe@mahe.com>
2026-01-02 22:20:31 +0530

    Adding unsold and original pool

--- src/components/Pending.jsx
+++ src/components/Pending.jsx
@@ -13,11 +13,14 @@ export default function Pending() {
     const displayCards = (elements) => {
 
         return (
-            <><div className="card-box">
-                {elements.map((item) => (
-                    <img src={item.img} key={item.id} style={{ width: '50px', height: 'auto', cursor: 'pointer' }} alt={item.name} onClick={() => setSelectedImg(item.img)} />
-                ))}
-            </div>
+            <>
+                <h3>{firstPool.length > 0 ? 'Original Pool' : 'Unsold Pool'}</h3>
+
+                < div className="card-box">
+                    {elements.map((item) => (
+                        <img src={item.img} key={item.id} style={{ width: '50px', height: 'auto', cursor: 'pointer' }} alt={item.name} onClick={() => setSelectedImg(item.img)} />
+                    ))}
+                </div >
                 <Modal visible={!!selectedImg} onClose={() => setSelectedImg(null)}>
                     <img src={selectedImg} style={{ width: '100%', height: 'auto' }} alt="Player" />
                 </Modal>
